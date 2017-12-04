d37edaac001e960c363730c2dba9744cb699f752
Subodh Kumar <subodh.kumar@cloudio.io>
2017-12-04 17:28:39 +0530

    Changes #2
    Changes again

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express=require('express');
+var path = require('path');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT,function(){
+    console.log('Boffo server started @'+PORT);
+});
+
+app.get('/',function(req,res){
+    res.redirect('pages/index.html');
+});
